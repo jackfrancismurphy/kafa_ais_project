8867466253fbecca82f0baba1118c0155a77f16f
Jack Murphy <jackfrancismurphy@outlook.com>
2023-04-17 14:29:38 +0100

    successfull consumer not attached to map.py

---
+++ config.py
@@ -0,0 +1,10 @@
+config = {
+     'bootstrap.servers': 'pkc-ymrq7.us-east-2.aws.confluent.cloud:9092',     
+     'security.protocol': 'SASL_SSL',
+     'sasl.mechanisms': 'PLAIN',
+     'sasl.username': 'FP4OKS6FFLHCTNL6', 
+     'sasl.password': 'skww7HWM/U0FkRP/DgX4NfPgFMBCS2J9jgg2RANOuMXPpL09ffZkSDsCYopuBBCg'}
+sr_config = {
+    'url': 'https://psrc-l6oz3.us-east-2.aws.confluent.cloud',
+    'basic.auth.user.info':'6LIRC7OBSHEVS3XF:e1zw9s0CjuUCIjY6mdYl08N41Hs7uaIxg3YHz9es8hORBaXPPw2kn1qcMwRhOWgh'
+}
--- consumer.py
+++ consumer.py
@@ -2,19 +2,15 @@ from confluent_kafka import Consumer, KafkaException
 from config import config
 
 def set_consumer_configs():
-    config['group.id'] = 'hello_group'
+    config['group.id'] = 'AIS_DATA'
     config['auto.offset.reset'] = 'earliest'
     config['enable.auto.commit'] = False
-    
-def assignment_callback(consumer, partitions):
-    for p in partitions:
-        print(f'Assigned to {p.topic}, partition {p.partition}')
-
 
 if __name__ == '__main__':
     set_consumer_configs()
     consumer = Consumer(config)
-    consumer.subscribe(['hello_topic'], on_assign=assignment_callback)    
+    consumer.subscribe(['pksqlc-qqdj2AIS_FILTERED_2'])
+
     try:
         while True:
             event = consumer.poll(1.0)
@@ -26,20 +22,13 @@ if __name__ == '__main__':
                 val = event.value().decode('utf8')
                 partition = event.partition()
                 print(f'Received: {val} from partition {partition}    ')
-                consumer.commit(event)        
+                consumer.commit(event)
+                break  # Stop polling after first message is received
     except KeyboardInterrupt:
         print('Canceled by user.')
     finally:
         consumer.close()
 
-    while True:
-        event = consumer.poll(1.0)
-        if event is None:
-            continue
-        if event.error():
-            raise KafkaException(event.error())
-        else:
-            val = event.value().decode('utf8')
-            partition = event.partition()
-            print(f'Received: {val} from partition {partition}    ')
-            # consumer.commit(event)
+    # Store the message value in a variable
+    message_value = val
+    print(f'Stored message value: {message_value}')
