49891315495555492d7c349ab48da87794d43999
Jack Murphy <jackfrancismurphy@outlook.com>
2023-04-17 16:02:50 +0100

    changes made pre-schema registry

--- map.py
+++ map.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 import ast
+from confluent_kafka import Consumer, KafkaException
+from config import config
 #import fileinput
 
 # on my windows machine $ echo [500, 100] | python3 \\wsl.localhost\Ubuntu\home\jackfmurphy\confluent_files\ais-demo\kafka_ais_project\map.py 
@@ -30,36 +32,70 @@ def convert_geographic_coordinate_to_pixel_value(lon, lat, transform):
     return refx, refy
 
 
+def set_consumer_configs():
+    config['group.id'] = 'AIS_DATA'
+    config['auto.offset.reset'] = 'earliest'
+    config['enable.auto.commit'] = False
+
+
+
+def topic_consumer():
+
+    set_consumer_configs()
+    consumer = Consumer(config)
+    consumer.subscribe(['pksqlc-qqdj2AIS_FILTERED_2'])
+
+    try:
+        while True:
+            event = consumer.poll(1.0)
+            if event is None:
+                continue
+            if event.error():
+                raise KafkaException(event.error())
+            else:
+                val = event.value().decode('utf8')
+                partition = event.partition()
+                print(f'Received: {val} from partition {partition}    ')
+                consumer.commit(event)
+                break  # Stop polling after first message is received
+    except KeyboardInterrupt:
+        print('Canceled by user.')
+    finally:
+        consumer.close()
+    
+    print(f'Stored message value: {val}')
+
+    return val
+
+
+    # Store the message value in a variable
+
 
 if __name__ == "__main__":
 
+    #variables
     transform = [0.0, 5, 0.0, 0, 0.0, 5]
-    
-    draw = []
-    #At a later stage this will need to be a dictionary
-
     alt = 0
     origin = 0
     running = True
+    draw = [] #At a later stage this will need to be a dictionary
 
-
-    #These lines below will change with the kcat output
-
-    input_str = input().strip()   
-    # Read standard input as a string
-    co_orindates = ast.literal_eval(input_str)  
-    # Parse string into a list
-    lon, lat = co_orindates   
-    # Unpack the list into two variables
-
-    #next(fileinput.input()) --> this will be used to get the next value, I believe. 
+    #data from the topic
+    boat = topic_consumer()
+    for i in boat:
+        print(i) 
+    lat, lon = boat["LAT"], boat["LON"]  
 
 
     pixlon,pixlat = convert_geographic_coordinate_to_pixel_value(lon, lat, transform)
 
     #In the future this will grab the next line of standard input and will be used for adding a new boat to the graph.
+    # def next_boat():
+    #     return random.randint(0, 895), random.randint(0,595)
+
+    #temp testing the input
     def next_boat():
-        return random.randint(0, 895), random.randint(0,595)
+         return lon, lat
 
 
     pygame.init()
